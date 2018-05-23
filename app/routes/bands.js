346820baecc6c7d42360b3e1286a93521e0c1388
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-05-22 21:36:16 -0500

    Agregando la base del objeto banda y creando objetos

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+
+var Band = EmberObject.extend({
+	name:'',
+});
 
 export default Route.extend({
+
+	model: function(){
+		var ledZepellin = Band.create({ name: 'Led Zepellin'});
+		var pearlJam = Band.create({ name: 'Pearl Jam'});
+		var fooFighters = Band.create({ name: 'Foo Fighters'});
+	
+		return [ledZepellin,pearlJam,fooFighters];
+	}
+
 });
